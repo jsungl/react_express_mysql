e8acbd060c0162a6abc4486f960a73d4c4086d92
jsungl <wotjd931026@gmail.com>
2022-10-04 19:08:44 +0900

    Board Component 생성

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import Topic from './topic';
+import Topic from './components/topic';
 
 
 function App() {
---
+++ src/components/Board.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const data = [
+    {1,'제목입니다1','user1'},
+    {2,'제목입니다2','user2'},
+    {3,'제목입니다3','user3'}
+];
+
+export default function Board() {
+
+    return (
+        <div>테이블</div>
+    )
+
+}
